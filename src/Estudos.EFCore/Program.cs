4cce2583e64bcb80194ea2e953ad733abf11b1a3
jorgelucas <jorgelucasl91@gmail.com>
2021-07-21 17:40:02 -0300

    [FEATURE] Ensure Deleted/Created

--- src/Estudos.EFCore/Program.cs
+++ src/Estudos.EFCore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Estudos.EFCore.Domain;
 
 namespace Estudos.EFCore
 {
@@ -7,6 +8,27 @@ namespace Estudos.EFCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //EnsureCreate();
+            EnsureDeleted();
         }
+
+        static void EnsureCreate()
+        {
+            using var db = new ApplicationDbContext();
+            //garante que o banco exista
+            //cria o banco caso não exista
+            db.Database.EnsureCreated();
+        }
+
+        static void EnsureDeleted()
+        {
+            using var db = new ApplicationDbContext();
+            //garante que o banco foi apagado
+            //dropa o banco caso exista
+            db.Database.EnsureDeleted();
+        }
+
+
     }
 }
