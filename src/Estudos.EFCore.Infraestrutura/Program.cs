57cfd7a1b4cc74dda9c0f9de6c966a4af6e54e63
jorgelucas <jorgelucasl91@gmail.com>
2021-07-26 10:48:31 -0300

    [FEATURE] Ciração do projeto de insfraestrutura
    - criação do projeto
    - criação das models
    - instalação dos pacotes

---
+++ src/Estudos.EFCore.Infraestrutura/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Estudos.EFCore.Infraestrutura.Data;
+
+namespace Estudos.EFCore.Infraestrutura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsultarDepartamentos();
+        }
+
+        static void ConsultarDepartamentos()
+        {
+            using var db = new ApplicationDbContext();
+
+            var departamentos = db.Departamentos.Where(p => p.Id > 0).ToArray();
+        }
+    }
+}
