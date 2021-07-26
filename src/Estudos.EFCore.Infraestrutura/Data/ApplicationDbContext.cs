1850fdb08711914a47f4cf33d9edb4cc1187b0fe
jorgelucas <jorgelucasl91@gmail.com>
2021-07-26 11:34:24 -0300

    [FEATURE] Gravando Logs em um arquivo

--- src/Estudos.EFCore.Infraestrutura/Data/ApplicationDbContext.cs
+++ src/Estudos.EFCore.Infraestrutura/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Estudos.EFCore.Infraestrutura.Domain;
 using Estudos.EFCore.Infraestrutura.Helper;
 using Microsoft.EntityFrameworkCore;
@@ -11,30 +12,36 @@ namespace Estudos.EFCore.Infraestrutura.Data
     public class ApplicationDbContext : DbContext
     {
         private readonly IConfiguration _configuration;
+        private readonly StreamWriter _writer;
 
         public ApplicationDbContext()
         {
             _configuration = ConfigurationHelper.ObterConfiguration();
+            var nomeArquivo = DateTime.Now.ToString("dd-MM-yyyy");
+            _writer = new StreamWriter($"log_{nomeArquivo}.txt", append: true);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
                 .UseSqlServer(_configuration.GetConnectionString("SqlServerConnection"))
-                //habilitando a exibição dos logs
-                //.LogTo(EscreverLogSql, LogLevel.Information)
+                .LogTo(_writer.WriteLine, LogLevel.Information);
 
 
-                .LogTo(
-                    //finção que escreve os logs no console
-                    EscreverLogSql,
-                    //tipos de log que deve ser exibidos
-                    new[] { RelationalEventId.CommandExecuted, CoreEventId.ContextInitialized },
-                    //nivel dos logs exibidos
-                    LogLevel.Information,
-                    //opções adicionais de como os logs deve ser exibidos
-                    DbContextLoggerOptions.LocalTime | DbContextLoggerOptions.SingleLine
-                    );
+            //habilitando a exibição dos logs
+            //.LogTo(EscreverLogSql, LogLevel.Information)
+
+
+            //.LogTo(
+            //    //finção que escreve os logs no console
+            //    EscreverLogSql,
+            //    //tipos de log que deve ser exibidos
+            //    new[] { RelationalEventId.CommandExecuted, CoreEventId.ContextInitialized },
+            //    //nivel dos logs exibidos
+            //    LogLevel.Information,
+            //    //opções adicionais de como os logs deve ser exibidos
+            //    DbContextLoggerOptions.LocalTime | DbContextLoggerOptions.SingleLine
+            //    )
         }
 
         public DbSet<Departamento> Departamentos { get; set; }
@@ -46,6 +53,12 @@ namespace Estudos.EFCore.Infraestrutura.Data
             Console.WriteLine(sql);
             Console.WriteLine("\n\n");
             Console.ResetColor();
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _writer.Dispose();
         }
     }
 }
