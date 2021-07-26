eb0de0402febe96f868616f6261b7318d67da2be
jorgelucas <jorgelucasl91@gmail.com>
2021-07-26 10:49:37 -0300

    [FEATURE] Configurando um log simplificado

---
+++ src/Estudos.EFCore.Infraestrutura/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Estudos.EFCore.Infraestrutura.Domain;
+using Estudos.EFCore.Infraestrutura.Helper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Estudos.EFCore.Infraestrutura.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext()
+        {
+            _configuration = ConfigurationHelper.ObterConfiguration();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(_configuration.GetConnectionString("SqlServerConnection"))
+                //habilitando a exibição dos logs
+                .LogTo(EscreverLogSql, LogLevel.Information);
+        }
+
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        public void EscreverLogSql(string sql)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(sql);
+            Console.WriteLine("\n\n");
+            Console.ResetColor();
+        }
+    }
+}
