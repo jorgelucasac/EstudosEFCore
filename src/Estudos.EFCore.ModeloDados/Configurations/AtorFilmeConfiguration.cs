b3b9ca47ad583b72ee00eac3ed4ab626b2ce8979
jorgelucas <jorgelucasl91@gmail.com>
2021-07-27 19:39:37 -0300

    [FEATURE] ustomização muitos-para-muitos
    - Informando o nome da tabela de ligação
    - informando o nome dos campos
    - criando campos adicionais

--- src/Estudos.EFCore.ModeloDados/Configurations/AtorFilmeConfiguration.cs
+++ src/Estudos.EFCore.ModeloDados/Configurations/AtorFilmeConfiguration.cs
@@ -11,12 +11,29 @@ namespace Estudos.EFCore.ModeloDados.Configurations
         public void Configure(EntityTypeBuilder<Ator> builder)
         {
             //forma simples
+            //builder
+            //    .HasMany(p => p.Filmes)
+            //    .WithMany(p => p.Atores)
+            //    .UsingEntity(p => p.ToTable("AtoresFilmes"));
+
             builder
                 .HasMany(p => p.Filmes)
                 .WithMany(p => p.Atores)
-                .UsingEntity(p => p.ToTable("AtoresFilmes"));
+                //nome da tabela e objeto de configurações
+                .UsingEntity<Dictionary<string, object>>(
+                    //nome da tabela
+                    "FilmesAtores",
 
-            
+                    //informa que filmes possui varios atoes e nomeia a chave estrangeira
+                    p => p.HasOne<Filme>().WithMany().HasForeignKey("FilmeId"),
+                    //informa que ator possui varios filmes e nomeia a chave estrangeira
+                    p => p.HasOne<Ator>().WithMany().HasForeignKey("AtorId"),
+                    p =>
+                    {
+                        //cria uma propriedade de sombra
+                        p.Property<DateTime>("CadastradoEm").HasDefaultValueSql("GETDATE()");
+                    }
+                );
         }
     }
 }
