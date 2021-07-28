164a0a6f191123e3a564dfc14e60aa17c0cb1361
jorgelucas <jorgelucasl91@gmail.com>
2021-07-28 10:03:16 -0300

    [FEATURE] Criação do peojeto DataAnnotations
    - instalação dos pacotes
    - configuração do DbContext

---
+++ src/Estudos.EFCore.DataAnnotations/Data/ApplicationDbContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Estudos.EFCore.DataAnnotations.Domain;
+using Estudos.EFCore.DataAnnotations.Helper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Estudos.EFCore.DataAnnotations.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext()
+        {
+            _configuration = ConfigurationHelper.ObterConfiguration();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(_configuration.GetConnectionString("SqlServerConnection"))
+                //habilitando detalhes de erros
+                .EnableDetailedErrors()
+                //habilitando visualização de dados sensiveis
+                .EnableSensitiveDataLogging()
+                //habilitando a exibição dos logs
+                .LogTo(EscreverLogSql, LogLevel.Information);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+            #region Aplicando Configurações
+
+            //forma 1
+            //modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+
+            //forma 2
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //forma 3
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+
+            modelBuilder.SharedTypeEntity<Dictionary<string, object>>("Configuracoes", b =>
+            {
+                b.Property<int>("Id");
+
+                b.Property<string>("Chave")
+                    .HasColumnType("VARCHAR(40)")
+                    .IsRequired();
+
+                b.Property<string>("Valor")
+                    .HasColumnType("VARCHAR(255)")
+                    .IsRequired();
+            });
+
+            #endregion
+
+        }
+
+        
+        public void EscreverLogSql(string sql)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(sql);
+            Console.WriteLine("\n\n");
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ src/Estudos.EFCore.DataAnnotations/Domain/Atributo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Estudos.EFCore.DataAnnotations.Domain
+{
+    [Table("TabelaAtributos")]
+    public class Atributo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("MinhaDescricao", TypeName = "VARCHAR(100)")]
+        public string Descricao { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Observação { get; set; }
+
+    }
+}
