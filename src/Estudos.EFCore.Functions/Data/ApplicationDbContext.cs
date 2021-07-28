56f390f26623dfe9c39ee343c41981011e0061f5
jorgelucas <jorgelucasl91@gmail.com>
2021-07-28 12:34:12 -0300

    [FEATURE] Criando o projeto de funções

---
+++ src/Estudos.EFCore.Functions/Data/ApplicationDbContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Estudos.EFCore.Functions.Domain;
+using Estudos.EFCore.Functions.Helper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Estudos.EFCore.Functions.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext()
+        {
+            _configuration = ConfigurationHelper.ObterConfiguration();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(_configuration.GetConnectionString("SqlServerConnection"))
+                //habilitando detalhes de erros
+                .EnableDetailedErrors()
+                //habilitando visualização de dados sensiveis
+                .EnableSensitiveDataLogging()
+                //habilitando a exibição dos logs
+                .LogTo(EscreverLogSql, LogLevel.Information);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+            #region Aplicando Configurações
+
+            //forma 1
+            //modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+
+            //forma 2
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //forma 3
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            #endregion
+
+        }
+
+        public DbSet<Funcao> Funcoes  { get; set; }
+        public void EscreverLogSql(string sql)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(sql);
+            Console.WriteLine("\n\n");
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ src/Estudos.EFCore.Functions/Helper/ConfigurationHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Estudos.EFCore.Functions.Helper
+{
+    public sealed class ConfigurationHelper
+    {
+        private readonly IConfiguration _config;
+        private static ConfigurationHelper _instancia;
+        private static readonly object Locker = new object();
+
+        private ConfigurationHelper()
+        {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public static IConfiguration ObterConfiguration()
+        {
+
+            if (_instancia == null)
+            {
+                lock (Locker)
+                {
+                    if (_instancia == null)
+                    {
+                        _instancia = new ConfigurationHelper();
+                    }
+                }
+            }
+
+
+            return _instancia._config;
+        }
+    }
+}
