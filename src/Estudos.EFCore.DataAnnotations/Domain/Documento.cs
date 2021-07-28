9525a31e75f09f7fb7c0970910c5290ef2bb2512
jorgelucas <jorgelucasl91@gmail.com>
2021-07-28 11:09:16 -0300

    [FEATURE] Atributo Backing Field

---
+++ src/Estudos.EFCore.DataAnnotations/Domain/Documento.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Estudos.EFCore.ModeloDados.Domain
+{
+    public class Documento
+    {
+        private string _cpf;
+
+        public int Id { get; set; }
+
+        public void SetCpf(string cpf)
+        {
+            // Validações
+            if (string.IsNullOrWhiteSpace(cpf))
+            {
+                throw new System.Exception("CPF Invalido");
+            }
+
+            _cpf = cpf;
+        }
+
+        
+        [BackingField(nameof(_cpf))]
+        public string CPF => _cpf;
+
+    }
+}
