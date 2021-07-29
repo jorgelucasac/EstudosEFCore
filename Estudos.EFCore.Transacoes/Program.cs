8961703d064073fb6f4a73c1cd5b09b98118bfd9
jorgelucas <jorgelucasl91@gmail.com>
2021-07-29 11:07:10 -0300

    [FEATURE] Salvando ponto de uma transação

--- Estudos.EFCore.Transacoes/Program.cs
+++ Estudos.EFCore.Transacoes/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Estudos.EFCore.Transacoes.Data;
 using Estudos.EFCore.Transacoes.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Estudos.EFCore.Transacoes
 {
@@ -11,7 +12,8 @@ namespace Estudos.EFCore.Transacoes
         {
             //ComportamentoPadrao();
             //GerenciandoTransacaoManualmente();
-            ReverterTransacao();
+            //ReverterTransacao();
+            SalvarPontoTransacao();
         }
 
         /// <summary>
@@ -115,6 +117,56 @@ namespace Estudos.EFCore.Transacoes
                 catch (Exception e)
                 {
                     transacao.Rollback();
+                }
+
+            }
+        }
+
+        static void SalvarPontoTransacao()
+        {
+            CadastrarLivro();
+
+            using (var db = new ApplicationDbContext())
+            {
+                using var transacao = db.Database.BeginTransaction();
+
+                try
+                {
+                    var livro = db.Livros.FirstOrDefault(p => p.Id == 1);
+                    livro.Autor = "Rafael Almeida";
+                    db.SaveChanges();
+
+                    //salva o estado da transção
+                    transacao.CreateSavepoint("desfazer_apenas_insercao");
+
+                    db.Livros.Add(
+                        new Livro
+                        {
+                            Titulo = "ASP.NET Core Enterprise Applications",
+                            Autor = "Eduardo Pires"
+                        });
+                    db.SaveChanges();
+
+                    db.Livros.Add(
+                        new Livro
+                        {
+                            Titulo = "Dominando o Entity Framework Core",
+                            Autor = "Rafael Almeida".PadLeft(16, '*')
+                        });
+                    db.SaveChanges();
+
+                    transacao.Commit();
+                }
+                catch (DbUpdateException e)
+                {
+                    //realizando o rollback para um ponto especifico
+                    transacao.RollbackToSavepoint("desfazer_apenas_insercao");
+
+                    //caso exista entidades add, realiza o commit 
+                    if (e.Entries.Count(p => p.State == EntityState.Added) == e.Entries.Count)
+                    {
+                        transacao.Commit();
+                    }
                 }
 
             }
