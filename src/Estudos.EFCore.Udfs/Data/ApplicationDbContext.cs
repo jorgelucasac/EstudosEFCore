3aab26a4f20f6be7a715a897f860889c346bc021
jorgelucas <jorgelucasl91@gmail.com>
2021-07-29 11:37:32 -0300

    [FEATURE] Criando o projeto de udfs

---
+++ src/Estudos.EFCore.Udfs/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Estudos.EFCore.Udfs.Domain;
+using Estudos.EFCore.Utils.Helper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Estudos.EFCore.Udfs.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext()
+        {
+            _configuration = ConfigurationHelper.ObterConfiguration();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(_configuration.GetConnectionString("SqlServerConnection"))
+                //habilitando detalhes de erros
+                .EnableDetailedErrors()
+                //habilitando visualização de dados sensiveis
+                .EnableSensitiveDataLogging()
+                //habilitando a exibição dos logs
+                .LogTo(EscreverLogSql, LogLevel.Information);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+
+        public DbSet<Livro> Livros { get; set; }
+
+        public void EscreverLogSql(string sql)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(sql);
+            Console.WriteLine("\n\n");
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ src/Estudos.EFCore.Udfs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estudos.EFCore.Udfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
