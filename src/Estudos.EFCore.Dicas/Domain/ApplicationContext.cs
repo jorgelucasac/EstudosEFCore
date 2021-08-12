ffe2d7c6e3d7cf262f06bd9553e162e42588eb5e
jorgelucas <jorgelucasl91@gmail.com>
2021-08-12 15:37:34 -0300

    [FEATURE] Preparando o ambiente

---
+++ src/Estudos.EFCore.Dicas/Domain/ApplicationContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Estudos.EFCore.Dicas.Data;
+using Estudos.EFCore.Utils.Helper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Estudos.EFCore.Dicas.Domain
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationContext()
+        {
+            _configuration = ConfigurationHelper.ObterConfiguration();
+        }
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Colaborador> Colaboradores { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(_configuration.GetConnectionString("SqlServerConnection"))
+                //habilitando detalhes de erros
+                .EnableDetailedErrors()
+                //habilitando visualização de dados sensiveis
+                .EnableSensitiveDataLogging()
+                //habilitando a exibição dos logs
+                .LogTo(EscreverLogSql, LogLevel.Information);
+        }
+
+        public void EscreverLogSql(string sql)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(sql);
+            Console.WriteLine("\n\n");
+            Console.ResetColor();
+        }
+    }
+}
