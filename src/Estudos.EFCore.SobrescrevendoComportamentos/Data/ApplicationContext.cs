89bb46ae866c2e5f1251f05a47e5497ce453ccec
jorgelucas <jorgelucasl91@gmail.com>
2021-08-12 18:10:45 -0300

    [FEATURE] Criando entidade e contexto

---
+++ src/Estudos.EFCore.SobrescrevendoComportamentos/Data/ApplicationContext.cs
@@ -0,0 +1,48 @@
+using System;
+using Estudos.EFCore.SobrescrevendoComportamentos.Entities;
+using Estudos.EFCore.Utils.Helper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Estudos.EFCore.SobrescrevendoComportamentos.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DbSet<Departamento> Departamentos { get; set; }
+
+        public ApplicationContext()
+        {
+            _configuration = ConfigurationHelper.ObterConfiguration();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(_configuration.GetConnectionString("SqlServerConnection"),
+                    opt =>
+                        //habilitando a quantidade de dados por comando enviados ao banco de dados
+                        opt.MaxBatchSize(50)
+                            //forcando o uso do SplitQuery
+                            .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery))
+                //informando o comportamento do Trackin para todas as consultas
+                //.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTrackingWithIdentityResolution)
+                //habilitando detalhes de erros
+                .EnableDetailedErrors()
+                //habilitando visualização de dados sensiveis
+                .EnableSensitiveDataLogging()
+                //habilitando a exibição dos logs
+                .LogTo(EscreverLogSql, LogLevel.Information);
+        }
+        
+
+        public void EscreverLogSql(string sql)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(sql);
+            Console.WriteLine("\n\n");
+            Console.ResetColor();
+        }
+    }
+}
